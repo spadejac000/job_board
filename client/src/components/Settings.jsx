c0f565db26d63311d79295f75d331be001683929
Jacob Spade <spade.jacob@Yahoo.com>
2022-01-02 14:44:29 -0800

    fixed stuff

--- client/src/components/Settings.jsx
+++ client/src/components/Settings.jsx
@@ -4,6 +4,7 @@ import {lightTheme, darkTheme, GlobalStyles} from '../themes';
 import '../css/settings.css'
 import {useDispatch, useSelector} from 'react-redux'
 import {updateTheme} from '../actions/themeActions'
+import {Card} from 'react-bootstrap'
 
 const Settings = () => {
 
@@ -27,12 +28,12 @@ const Settings = () => {
   return (
     <div className="container">
       <h1>Settings</h1>
-      <div className="theme-mode-container">
-        <h1 className="theme-mode-title">Light / Dark Mode</h1>
+      <Card className="theme-mode-container">
+        <h3 className="theme-mode-title">Light / Dark Mode</h3>
         <div className="toggle-container">
           <input className="toggle-theme-input" type="checkbox" id="switch" name="theme" checked={themeToggleChecked} onChange={() => themeToggler()}/><label className="theme-label" for="switch">Toggle</label>
         </div>
-      </div>
+      </Card>
     </div>
   )
 }

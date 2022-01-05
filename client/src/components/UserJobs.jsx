ad2b1b7616bd6a2d0f7f7b201d80dcca36bb1291
Jacob Spade <spade.jacob@Yahoo.com>
2022-01-05 14:49:53 -0800

    a;lsdkfj

--- client/src/components/UserJobs.jsx
+++ client/src/components/UserJobs.jsx
@@ -1,7 +1,9 @@
 import React, {useEffect} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import {getUserJobs} from '../actions/jobActions'
-import {Card, Container, Badge} from 'react-bootstrap'
+import {Card, Container, Badge, Button, Col, Row} from 'react-bootstrap'
+import {FaEdit, FaTimes} from 'react-icons/fa'
+import '../css/user-jobs.css'
 
 const UserJobs = () => {
 
@@ -34,6 +36,10 @@ const UserJobs = () => {
           <div><p>Job Location</p><Badge>{job.job_location}</Badge></div>
 
           <p>{job._description}</p>
+          <div className="user-job-actions-container">
+            <Button variant="warning"><FaEdit/> Edit</Button>
+            <Button variant="danger"><FaTimes/> Delete</Button>
+          </div>
         </Card>
       ))}
     </Container>

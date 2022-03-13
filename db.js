e7e1f3cf71488532bac7e6d76d4f6ee5214a2548
Jacob Spade <spade.jacob@Yahoo.com>
2022-03-13 14:10:42 -0700

    setting up database for production

--- db.js
+++ db.js
@@ -1,11 +1,18 @@
 const Pool = require('pg').Pool
+require('dotenv').config()
 
-const pool = new Pool({
-  user: "jacobspade",
-  password: "postgres",
-  host: "localhost",
-  port: 5432,
-  database: "jobboard"
-})
+const devConfig = {
+  user: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  host: process.env.PG_HOST,
+  port: process.env.PG_PORT,
+  database: process.env.PG_DATABASE
+}
+
+const proConfig = {
+  connectionString: process.env.DATABASE_URL // Heroku addon
+}
+
+const pool = new Pool(process.env.NODE_ENV === 'production' ? proConfig : devConfig)
 
 module.exports = pool

48117a60d6bcbc447db06e646537f7ea52f011cd
Jacob Spade <spade.jacob@Yahoo.com>
2022-03-15 10:04:41 -0700

    setting up database squema for heroku

--- database.sql
+++ database.sql
@@ -1,4 +1,6 @@
-CREATE DATABASE jobboard;
+-- CREATE DATABASE jobboard;
+
+CREATE EXTENSION IF NOT EXISTS  "uuid-ossp";
 
 CREATE TABLE users(
   user_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
@@ -18,14 +20,15 @@ CREATE TABLE jobs(
   job_location VARCHAR(255),
   job_type VARCHAR(255),
   salary VARCHAR(255),
-  benefits VARCHAR(255),
   _description VARCHAR(10000),
-  user_id uuid REFERENCES users(user_id)
+  company_name VARCHAR(255) NOT NULL,
+  date_posted DATE NOT NULL,
+  user_id uuid REFERENCES users(user_id), UNIQUE(user_id)
 );
 
 CREATE TABLE favorite_jobs(
   favorite_jobs_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-  user_id uuid REFERENCES users(user_id),
+  user_id uuid REFERENCES users(user_id) NOT NULL,
   job_id uuid REFERENCES jobs(job_id)
 );
 
@@ -48,10 +51,4 @@ CREATE TABLE benefits(
   vision_insurance VARCHAR(255)
 );
 
-INSERT INTO users (user_first_name, user_last_name, user_email, user_password) VALUES ('henry', 'smith', 'henry@gmail.com', '123456');
-
-INSERT INTO jobs (job_title, work_address, city, _state, zip, job_location, job_type, salary, _description) VALUES ('Web Developer', '1234 5th street ne', 'Seattle', 'washington', '12345', 'remote', 'full-time', '$100,000 a year', 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus nesciunt mollitia possimus perferendis nisi. Nobis aperiam eius blanditiis ipsum amet beatae optio cumque labore voluptas, hic placeat tenetur rerum quia.');
-
-ALTER TABLE users ADD favorite_jobs text[];
-
-INSERT INTO users VALUES ARRAY[]
+ALTER TABLE jobs ADD COLUMN benefits_id uuid REFERENCES benefits(benefits_id);
